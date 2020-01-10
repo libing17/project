1c2393760389fb0c85198aa2ab7ef05f88f78253
libing17 <59722375+libing17@users.noreply.github.com>
2020-01-10 12:33:46 +0530

    Create learning.c

---
+++ learning.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{float a,b,c;
+float  d;
+    printf("enter the values\n");
+    scanf("%f %f",&a,&b);
+    c=a+b;
+    d=a-b;
+    printf("sum %f",c);
+    printf("difference %f",d);
+   return 0;
+
+
+}
