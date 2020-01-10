6a1a812b44e7d17d8d1776c3a0626f3ebcd0ecb9
libing17 <59722375+libing17@users.noreply.github.com>
2020-01-10 12:45:13 +0530

    functions added to the program

--- learning.c
+++ learning.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+void add(float,float);
+void sub(float,float);
 int main()
 {float a,b,c;
 float  d;
     printf("enter the values\n");
     scanf("%f %f",&a,&b);
-    c=a+b;
-    d=a-b;
-    printf("sum %f",c);
-    printf("difference %f",d);
+    add(a,b);
+    sub(a,b);
    return 0;
-
-
+}
+void add(float a,float b)
+{
+  float c;
+ c=a+b;
+ printf("sum %f",c);
+}
+void sub(float a,float b)
+{
+  float d;
+   d=a-b;
+   printf("difference %f",d);
 }
